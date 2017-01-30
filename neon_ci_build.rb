e377e4867d6963df8eaf74e630738f5bc1eb96ae
Harald Sitter <sitter@kde.org>
2017-01-30 18:07:31 +0100

    on second thought don't ignore errors but make sure snapcraft is current

--- neon_ci_build.rb
+++ neon_ci_build.rb
@@ -33,6 +33,7 @@ end
 system('apt-key', 'adv',
        '--keyserver', 'keyserver.ubuntu.com',
        '--recv', ARCHIVE_KEY) || raise
-system('apt', 'update') # ignore errors here. snapcraft will handle it.
+system('apt', 'update') || raise
+system('apt', 'install', '-y', 'snapcraft') || raise
 
 system('snapcraft', chdir: builddir) || raise
