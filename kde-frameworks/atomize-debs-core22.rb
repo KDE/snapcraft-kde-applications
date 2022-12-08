818fa70d087c1d5a14fedc1d1a2dbed389c651bb
Scarlett Moore <sgmoore@kde.org>
2022-12-08 09:21:05 -0700

    kde-frameworks: fix neon repo in atomize debs.

--- kde-frameworks/atomize-debs-core22.rb
+++ kde-frameworks/atomize-debs-core22.rb
@@ -337,7 +337,7 @@ config.summary = 'KDE Frameworks 5'
 config.description = 'KDE Frameworks are addons and useful extensions to Qt'
 config.confinement = 'strict'
 config.grade = 'stable'
-config.base = 'core20'
+config.base = 'core22'
 config.compression = 'lzo'
 
 slot = SnapcraftConfig::Slot.new
@@ -351,7 +351,7 @@ package_repo.type = 'apt'
 package_repo.components = %w[main]
 package_repo.suites = %w[jammy]
 package_repo.key_id = '444DABCF3667D0283F894EDDE6D4736255751E5D'
-package_repo.url = 'http://origin.archive.neon.kde.org/user'
+package_repo.url = 'http://origin.archive.neon.kde.org/unstable'
 package_repo.key_server = 'keyserver.ubuntu.com'
 config.package_repositories.push(package_repo)
 
