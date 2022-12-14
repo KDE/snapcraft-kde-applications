9c02281c70d7a81a8df16470322e8d4011871ee0
Scarlett Moore <sgmoore@kde.org>
2022-12-14 09:24:35 -0700

    kde-frameworks-core22: use 3 on kf5 version split as we are past 100.

--- kde-frameworks-core22/atomize-debs-core22.rb
+++ kde-frameworks-core22/atomize-debs-core22.rb
@@ -456,7 +456,7 @@ end
 
 # Construct a new interface name with up to date versions.
 # This is the only way we can version a content snap.
-kf5_version = 'kde-frameworks-' + kf5_version.split('.')[0..1].join('-')
+kf5_version = 'kde-frameworks-' + kf5_version.split('.')[0..2].join('-')
 qt5_version = 'qt-' + qt5_version.split('.')[0..1].join('-')
 platform_version = 'core22'
 
