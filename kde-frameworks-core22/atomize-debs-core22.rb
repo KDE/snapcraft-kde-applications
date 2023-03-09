825438deb28c7050380682a3d07339e67be32907
Scarlett Moore <sgmoore@debian.org>
2023-03-09 09:57:32 -0700

    Versions still are not right. Hard code it all.

--- kde-frameworks-core22/atomize-debs-core22.rb
+++ kde-frameworks-core22/atomize-debs-core22.rb
@@ -451,8 +451,10 @@ end
 
 # Construct a new interface name with up to date versions.
 # This is the only way we can version a content snap.
-kf5_version = 'kf5-' + kf5_version.split('.')[0..1].join('-')
-qt5_version = 'qt-' + qt5_version.split('.')[0..1].join('-')
+#kf5_version = 'kf5-' + kf5_version.split('.')[0..1].join('-')
+#qt5_version = 'qt-' + qt5_version.split('.')[0..0].join('-')
+kf5_version = 'kf5-5-103'
+qt5_version = 'qt-5-15-8'
 platform_version = 'core22'
 
 latest_version = [kf5_version, qt5_version, platform_version].join('-')
